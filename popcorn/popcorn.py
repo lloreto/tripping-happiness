f9f7717f6bf48f36edd6047f59a3a0bdcd440a76
lloreto <lloreto@gmail.com>
2015-07-05 16:24:20 +0800

    Added word2vec implementation

--- popcorn/popcorn.py
+++ popcorn/popcorn.py
@@ -1,40 +1,102 @@
 #Python code for bag of words popcorn challenge
-
 import pandas as pd
 from bs4 import BeautifulSoup
-
-train = pd.read_csv(r'data\labeledTrainData.tsv',header=0,delimiter='\t',quoting=3)
-
-# Import BeautifulSoup into your workspace
-from bs4 import BeautifulSoup             
-
-# Initialize the BeautifulSoup object on a single movie review     
-example1 = BeautifulSoup(train["review"][0])  
-
-# Print the raw review and then the output of get_text(), for 
-# comparison
-print(train["review"][0])
-print(example1.get_text())
-
 import re
-# Use regular expressions to do a find-and-replace
-letters_only = re.sub("[^a-zA-Z]",           # The pattern to search for
-                      " ",                   # The pattern to replace it with
-                      example1.get_text() )  # The text to search
-print(letters_only)
-
-lower_case = letters_only.lower()        # Convert to lower case
-words = lower_case.split()               # Split into words
-
+from nltk.corpus import stopwords
 import nltk
-nltk.download()  # Download text data sets, including stop words
 
-from nltk.corpus import stopwords # Import the stop word list
-print(stopwords.words("english"))
+# Read data from files 
+def load_data():
+    train = pd.read_csv( "data/labeledTrainData.tsv", header=0, 
+     delimiter="\t", quoting=3 )
+    test = pd.read_csv( "data/testData.tsv", header=0, delimiter="\t", quoting=3 )
+    unlabeled_train = pd.read_csv( "data/unlabeledTrainData.tsv", header=0, 
+     delimiter="\t", quoting=3 )
 
-# Remove stop words from "words"
-words = [w for w in words if not w in stopwords.words("english")]
-print(words)
+    # Verify the number of reviews that were read (100,000 in total)
+    print("Read %d labeled train reviews, %d labeled test reviews, " \
+     "and %d unlabeled reviews\n" % (train["review"].size,
+     test["review"].size, unlabeled_train["review"].size ))
+
+    return (train, test, unlabeled_train)
+
+def review_to_wordlist(review, remove_stopwords = False):
+    # 1/ Remove HTML
+    review_text = BeautifulSoup(review).get_text()
+    # 2/ Remove non letters
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    # 3/ Convert to lower case and split
+    words = review_text.lower().split()
+    # 4/ Optionally remove stopwords
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    return(words)
 
 
+# Define a function to split a review into parsed sentences
+def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, \
+              remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
 
+def run_popcorn():
+    #load data
+    train, test, unlabeled_train = load_data()
+
+    # Load the punkt tokenizer
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    sentences = []  # Initialize an empty list of sentences
+    print("Parsing sentences from training set")
+    for review in train["review"]:
+        sentences += review_to_sentences(review, tokenizer)
+
+    print("Parsing sentences from unlabeled set")
+    for review in unlabeled_train["review"]:
+        sentences += review_to_sentences(review, tokenizer)
+
+    # Import the built-in logging module and configure it so that Word2Vec 
+    # creates nice output messages
+    import logging
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+        level=logging.INFO)
+
+    # Set values for various parameters
+    num_features = 300    # Word vector dimensionality                      
+    min_word_count = 40   # Minimum word count                        
+    num_workers = 4       # Number of threads to run in parallel
+    context = 10          # Context window size                                                                                    
+    downsampling = 1e-3   # Downsample setting for frequent words
+
+    # Initialize and train the model (this will take some time)
+    from gensim.models import word2vec
+    print("Training model...")
+    model = word2vec.Word2Vec(sentences, workers=num_workers, \
+                size=num_features, min_count = min_word_count, \
+                window = context, sample = downsampling)
+
+    # If you don't plan to train the model any further, calling 
+    # init_sims will make the model much more memory-efficient.
+    model.init_sims(replace=True)
+
+    # It can be helpful to create a meaningful model name and 
+    # save the model for later use. You can load it later using Word2Vec.load()
+    model_name = "300features_40minwords_10context"
+    model.save(model_name)
+    return model
